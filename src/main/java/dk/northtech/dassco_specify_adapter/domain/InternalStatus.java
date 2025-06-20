1b40db6942ada00ca40848af2cecc3fe619fe5ab
Thomas <Thomas@northtech.dk>
2025-06-20 16:39:40 +0200

    bugfix: adds missing status

--- src/main/java/dk/northtech/dassco_specify_adapter/domain/InternalStatus.java
+++ src/main/java/dk/northtech/dassco_specify_adapter/domain/InternalStatus.java
@@ -3,9 +3,11 @@ package dk.northtech.dassco_specify_adapter.domain;
 public enum InternalStatus {
     METADATA_RECEIVED
     , ASSET_RECEIVED
-    , ERDA_SYNCHRONISED
+    , COMPLETED
     , ERDA_FAILED
-    , ERDA_ERROR
-    ,SPECIFY_SYNC_SCHEDULED
-    ,SPECIFY_SYNC_FAILED
+    //   , ERDA_ERROR
+    , ERDA_SYNCHRONISED
+    , SPECIFY_SYNC_SCHEDULED
+    , SPECIFY_SYNC_FAILED
+    , SPECIFY_SYNCHRONISED
 }
