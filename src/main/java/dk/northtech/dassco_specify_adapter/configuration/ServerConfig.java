79eaa5bee60da34991c789d9a9eb840b6011fe82
Christoffer Hansen <christofferhansen20@gmail.com>
2026-06-11 17:16:03 +0200

    fix(web_asset_store.xml): now returns the correct hostname

--- src/main/java/dk/northtech/dassco_specify_adapter/configuration/ServerConfig.java
+++ src/main/java/dk/northtech/dassco_specify_adapter/configuration/ServerConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "specify-bridge")
 public record ServerConfig(
-        String rootUr
+        String rootUrl
 ) {
 
 }
